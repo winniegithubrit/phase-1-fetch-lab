ca337eac50c7c76ca3d1108da2ad54629b67bde3
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-25 21:17:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((res) => res.json())
+  .then((json) => renderBooks(json));
   
 }
 
